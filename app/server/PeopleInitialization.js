51c8e11545e8aa9d6f45d486efce9f2aa83bf1c3
Nathan Onaka <nateonaka@gmail.com>
2018-03-06 20:25:16 -1000

    Task 3

---
+++ app/server/PeopleInitialization.js
@@ -0,0 +1,6 @@
+import { people } from '../both/PeopleCollection.js';
+
+people.insert({ first: 'Philip', last: 'Johnson', age: 60, city: 'Kailua'});
+people.insert({ first: "Joanne", last: "Amberg", age: 58, city: "Kailua" });
+people.insert({ first: "Jenna", last: "Corin", age: 31, city: "Boulder Creek" });
+people.insert({ first: "Katie", last: "Kai", age: 25, city: "Palo Alto" });
